e9cbc51e2bdbab1a296781e204b5e55655b26f54
Ivan Chan <ivanccy04@gmail.com>
2024-12-17 12:04:06 +0100

    Project page update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,22 +7,31 @@ const Projects = () => {
         {
             id: 1,
             name: "Chun Amsterdam",
-            description: "design, development",
+            description: "Design, Development",
             image: chunPreview,
         },
         {
             id: 2,
-            name: "portfolio website",
-            description: "design, development",
+            name: "Portfolio Website",
+            description: "Design, Development",
             image: logo,
         },
     ];
 
     return (
         <section className="w-full min-h-screen bg-white py-16" id="projects">
-            <div className="flex flex-col gap-16 ">
-                {projects.map((project) => (
-                    <div key={project.id} className="mx-auto">
+            <div
+                className="grid grid-cols-2 md:grid-cols-4 gap-4 px-4 lg:px-8"
+            >
+                {projects.map((project, index) => (
+                    <div
+                        key={project.id}
+                        className={`${
+                            index === 0
+                                ? "col-span-2 row-span-2"
+                                : "col-span-1 row-span-1"
+                        } bg-white rounded-lg shadow-md`}
+                    >
                         <ProjectCard
                             image={project.image}
                             name={project.name}
