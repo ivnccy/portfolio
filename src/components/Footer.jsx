f204a0238e56ab9c97e77cd8047c3dec6e2ec261
Ivan Chan <ivanccy04@gmail.com>
2024-12-17 19:29:06 +0100

    Footer refactor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,28 +10,16 @@ function Footer() {
             </section>
 
             <footer className="w-full py-4 text-sm" id="footer">
-                <div className="flex justify-between items-center px-4">
+                <div className="flex justify-between items-center px-4 text-gray-500 no-underline hover:text-gray-700">
                     <div>
                         <p className="text-gray-500">&copy; {new Date().getFullYear()} Ivan Chan</p>
                     </div>
                     <div className="flex gap-2">
-                        <a
-                            href="https://www.linkedin.com/in/ivanccy/"
-                            className="text-gray-500 no-underline hover:text-gray-700"
-                        >LinkedIn
-                        </a>
-                        <a
-                            href="https://github.com/ivnccy"
-                            className="text-gray-500 no-underline hover:text-gray-700"
-                        >GitHub
-                        </a>
+                        <a href="https://www.linkedin.com/in/ivanccy/">LinkedIn</a>
+                        <a href="https://github.com/ivnccy">GitHub</a>
                     </div>
                     <div>
-                        <a
-                            href="mailto:ivanccy04@gmail.com"
-                            className="text-black no-underline"
-                        > ivanccy04@gmail.com
-                        </a>
+                        <a href="mailto:ivanccy04@gmail.com"> ivanccy04@gmail.com</a>
                     </div>
                 </div>
             </footer>
