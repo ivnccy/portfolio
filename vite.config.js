d29894d2d5ab208b057de31da70210e97e9fdb03
Ivan Chan <ivanchan5786@icloud.com>
2025-02-20 10:41:46 +0100

    Config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ivnccy.github.io/",
+  base: "/",
   plugins: [
     vue(),
     tailwindcss(),
